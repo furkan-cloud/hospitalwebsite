bf9cd62aacf9dc57a87af421d15fe8700bc7734f
furkan-cloud <firstinfirstout2020@gmail.com>
2021-03-31 17:58:04 +0300

    header updated

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from '../../assets/images/logo2.png';
 
 const Header = () => {
-  return (
-    <div id="header_container">
-      <div id="logo">
-        <img src="" alt="" />
-      </div>
-      <div></div>
-    </div>
-  );
-};
+    return (
+        <div id="header_container">
+            <div id="logo">
+                <Link to="/"><img src={Logo} width="200" height="85"></img></Link>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/about">ABOUT</a></li>
+                    <li><a href="/contact">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
 
-export default Header;
+export default Header
