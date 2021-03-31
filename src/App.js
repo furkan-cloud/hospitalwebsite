711cc3673098df9421a1b6c789a33562177dc90c
furkan-cloud <firstinfirstout2020@gmail.com>
2021-03-31 17:49:02 +0300

    app.js updated, logos added, global css updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Detail from "./pages/Detail";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./pages/Detail/Detail";
+import Home from "./pages/Home/Home";
 import Header from "./components/header/index";
 import Footer from "./components/footer";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Home />
-        <Detail />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/detail/:id" component={Detail} />
       </Switch>
       <Footer />
     </Router>
